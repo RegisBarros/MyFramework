537cccd366891c38f9f3243023ed6b97190890a9
Reginaldo Alves de Barros <R21Regis@hotmail.com>
2019-04-13 18:00:45 -0300

    Add project files.

---
+++ src/MyFramework.Logger/Log.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MyFramework.Logger
+{
+    public class Log
+    {
+        public async Task Error(Exception ex)
+        {
+            // do something nice
+        }
+
+        public async Task Info(string message)
+        {
+            // do something nice
+        }
+    }
+}
---
+++ src/MyFramework.SmtpServer/Smtp.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+
+namespace MyFramework.SmtpServer
+{
+    public class Smtp
+    {
+        public async Task Send(Email email)
+        {
+            // do something nice
+        }
+    }
+
+    public class Email
+    {
+        public Email(string from, string to, string body, string subject, byte[] attachement)
+        {
+            From = from;
+            To = to;
+            Body = body;
+            Subject = subject;
+            Attachement = attachement;
+        }
+
+        public string From { get; private set; }
+        public string To { get; private set; }
+        public string Body { get; private set; }
+        public string Subject { get; private set; }
+        public byte[] Attachement { get; private set; }
+    }
+}
